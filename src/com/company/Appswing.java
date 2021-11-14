b3a3cc56c100352e02be6f6b3e18f87ab7c80b8f
ZahraEjaz96 <zahraejaz456@gmail.com>
2021-11-14 17:56:41 +0500

    Added files

---
+++ src/com/company/Appswing.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Appswing {
+    private JButton Stop;
+    private JPanel panel1;
+    private JButton Play;
+    private JButton Reset;
+    private JButton ViewState;
+    private JButton SaveState;
+
+    public Appswing() {
+        Play.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Stop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ViewState.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        SaveState.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        JFrame frame = new JFrame();
+        frame.setTitle("Game Of Life");
+    }
+}
