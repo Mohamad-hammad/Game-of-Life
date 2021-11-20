a91350e861bddcfa05afc014190f44e5511ae3d0
Siddiqui23 <l192222@lhr.nu.edu.pk>
2021-11-20 14:31:05 +0500

    game logic

---
+++ src/Elements/Box.java
@@ -0,0 +1,76 @@
+package Elements;
+
+public class Box {
+    private boolean state;
+    private int YBox;
+    private int XBox;
+
+    public Box()
+    {
+        state = false;
+        System.out.println("Box Created");
+    }
+    public Box(int x, int y)
+    {
+    	state = false;
+    	XBox = x;
+    	YBox = y;
+    	System.out.println("Box with " + x + "," + y +" Created");
+    }
+    public void SetCoordinates(int x, int y)
+    {
+    	XBox = x;
+    	YBox = y;
+    }
+    public int GetX()
+    {
+    	return XBox;
+    }
+    public int GetY()
+    {
+    	return YBox;
+    }
+    public void SetAlive()
+    {
+    	state = true;
+    }
+    public void SetDead()
+    {
+    	state = false;
+    }
+    public boolean IsAlive()
+    {
+    	if (state == true)
+    		return true;
+    	else return false;
+    }
+    public boolean GetState()
+    {
+    	return state;
+    }
+    public boolean IsTop()
+    {
+    	if (YBox == 0)
+    		return true;
+    	else return false;
+    }
+    public boolean IsBottom(int height)
+    {
+    	if (YBox == height)
+    		return true;
+    	else return false;
+    }
+    public boolean IsLeft()
+    {
+    	if (XBox == 0)
+    		return true;
+    	else return false;
+    }
+    public boolean IsRight(int width)
+    {
+    	if (XBox == width)
+    		return true;
+    	else return false;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import Elements.*;
+
+public class Main {
+
+    public static void main(String[] args) 
+    {
+    	Game game = new Game();
+
+    }
+}
+
+
+/*class BusinessLogic {
+    BusinessLogic(){}
+    public void Start(){}
+    public void Stop(){}
+    public void MarkAlive(){}
+    public void MarkDead(){}
+    public int CheckBoundary(){}
+    public int CountAliveNeighbours(){}
+    public void NextGeneration(){}
+}
+
+class Game{
+    Game(){}
+
+}
+
+
+
+
+*/
