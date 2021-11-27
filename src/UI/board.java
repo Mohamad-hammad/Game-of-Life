7f3764b79a6767052ec788880ef157ddc8ad8d4d
ZahraEjaz96 <88451008+ZahraEjaz96@users.noreply.github.com>
2021-11-27 19:44:59 +0500

    Update board.java

--- src/UI/board.java
+++ src/UI/board.java
@@ -23,5 +23,43 @@ public class board {
             for (int j = 0; j < box[i].length; j++)
                 box[i][j] = " - ";
     }
+    public  void printBoard() {
+        System.out.print("    |");
+        for (int i = 0; i < box[0].length; i++){
+            System.out.print("    ");
+            System.out.print((char)('A' + i));
+            System.out.print("    |");
+        }
+        System.out.println();
+        for (int i = 0; i < box.length; i++){
+            System.out.print("----+");
+            for (int j = 0; j < box[0].length; j++){
+                System.out.print("---------+");
+            }
+            System.out.println();
+            System.out.print("  " + (i + 1) + " |");
+            for (int j = 0; j < box[0].length; j++){
+                if (box[i][j].length() < 10){
+                    int spaces = (9 - box[i][j].length()) / 2;
+                    for (int k = 0; k < spaces; k++){
+                        System.out.print(" ");
+                    }
+                    System.out.print(box[i][j]);
+                    for (int k = 0; k < (9 - box[i][j].length()) - spaces; k++){
+                        System.out.print(" ");
+                    }
+                }
+                else{
+                    System.out.print(box[i][j].substring(0, 9));
+                }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+    public void setAlive(int x, int y)
+    {
+        box[x-1][y-1]=" * ";
+    }
     
 }
