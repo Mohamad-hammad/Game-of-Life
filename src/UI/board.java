f394abdf6662372e3738a519010342dd18c38ee5
ZahraEjaz96 <88451008+ZahraEjaz96@users.noreply.github.com>
2021-11-28 23:05:16 +0500

    Create board.java

---
+++ src/UI/board.java
@@ -0,0 +1,142 @@
+package elements;
+import java.lang.Thread;
+import java.io.*;
+public class board {
+    public int height,width;
+    public String[][] box ;
+    int nextstate[][];
+    public board()
+    {
+
+    }
+    public board(int h,int w,int current[][])
+    {
+        height=h;
+        width=w;
+        //CurrentX =w;
+        //CurrentY =h;
+       box = new String[height][width];
+        for (int i = 0; i < box.length; i++)
+            for (int j = 0; j < box[i].length; j++) {
+                box[i][j] = " - ";
+                current[i][j]= 0;
+            }
+    }
+    public void resetBoard(int current[][])
+    {
+        for (int i = 0; i < box.length; i++)
+            for (int j = 0; j < box[i].length; j++) {
+                box[i][j] = " - ";
+               current[i][j] = 0;
+            }
+    }
+    public  void printBoard() {
+        System.out.print("    |");
+        for (int i = 0; i < box[0].length; i++){
+            System.out.print("    ");
+            System.out.print((char)('A' + i));
+            System.out.print("    |");
+        }
+        System.out.println();
+        for (int i = 0; i < box.length; i++){
+            System.out.print("----+");
+            for (int j = 0; j < box[0].length; j++){
+                System.out.print("---------+");
+            }
+            System.out.println();
+            System.out.print("  " + (i + 1) + " |");
+            for (int j = 0; j < box[0].length; j++){
+                if (box[i][j].length() < 10){
+                    int spaces = (9 - box[i][j].length()) / 2;
+                    for (int k = 0; k < spaces; k++){
+                        System.out.print(" ");
+                    }
+                    System.out.print(box[i][j]);
+                    for (int k = 0; k < (9 - box[i][j].length()) - spaces; k++){
+                        System.out.print(" ");
+                    }
+                }
+                else{
+                    System.out.print(box[i][j].substring(0, 9));
+                }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+    public void setAlive(int x, int y,int current[][])
+    {
+        box[x-1][y-1]=" * ";
+        current[x-1][y-1]=1;
+    }
+    public void viewState(int current[][])
+    {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if(current[i][j]==0){
+                    System.out.print("-");
+                }
+                if(current[i][j]==1)
+                {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+    private int decide(int i, int j,int current[][]){
+        int neighbors = 0;
+        if(j > 0){
+            if(current[i][j-1]==1) neighbors++;
+            if(i>0) if(current[i-1][j-1]==1) neighbors++;
+            if(i<height-1) if(current[i+1][j-1]==1) neighbors++;
+        }
+        if(j < width-1){
+            if(current[i][j+1]==1) neighbors++;
+            if(i>0) if(current[i-1][j+1]==1) neighbors++;
+            if(i<height-1) if(current[i+1][j+1]==1) neighbors++;
+        }
+        if(i>0) if(current[i-1][j]==1) neighbors++;
+        if(i<height-1) if(current[i+1][j]==1) neighbors++;
+        if(neighbors == 3) return 1;
+        if(current[i][j]==1 && neighbors == 2) return 1;
+        return 0;
+    }
+    public void nextState(int current[][])
+    {
+        nextstate=new int[height][width];
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                nextstate[i][j] = decide(i,j,current);
+            }
+        }
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                current[i][j] = nextstate[i][j];
+                if(current[i][j]==1)
+                {
+                    box[i][j]="*";
+                }
+            }
+        }
+    }
+    public final static void clearConsole()
+    {
+        try
+        {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows"))
+            {
+                Runtime.getRuntime().exec("cls");
+            }
+            else
+            {
+                Runtime.getRuntime().exec("clear");
+            }
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
